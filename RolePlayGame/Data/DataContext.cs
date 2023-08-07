862b4a81225c575c2fa976dd3b2f7f0c0d783cbe
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-07 13:10:25 +0330

    add Weapon model and relation with character

--- RolePlayGame/Data/DataContext.cs
+++ RolePlayGame/Data/DataContext.cs
@@ -8,4 +8,5 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     public DbSet<Character> Characters { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<Weapon> Weapons { get; set; }
 }
