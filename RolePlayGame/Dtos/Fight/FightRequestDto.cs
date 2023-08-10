e94772c9bc9f8bcaa2b9b2241dd407595b08f594
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-10 23:08:36 +0330

    add DTOs for fight (deathmatch)

---
+++ RolePlayGame/Dtos/Fight/FightRequestDto.cs
@@ -0,0 +1,6 @@
+﻿namespace RolePlayGame.Dtos.Fight;
+
+public class FightRequestDto
+{
+    public List<int> CharacterIds { get; set; }
+}
---
+++ RolePlayGame/Dtos/Fight/FightResultDto.cs
@@ -0,0 +1,6 @@
+﻿namespace RolePlayGame.Dtos.Fight;
+
+public class FightResultDto
+{
+    public List<string> Log { get; set; } = new List<string>();
+}
