9ebd658da0142faacdd3211bf459713afb419edf
Zahra Masoumi <zahraamasoumi@gmail.com>
2023-08-10 20:13:33 +0330

    update character model and get dto with fight properties

--- RolePlayGame/Migrations/20230810164223_FightProperties.Designer.cs
+++ RolePlayGame/Migrations/20230810164223_FightProperties.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RolePlayGame.Data;
 
@@ -10,9 +11,11 @@ using RolePlayGame.Data;
 namespace WebApplication4.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20230810164223_FightProperties")]
+    partial class FightProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
